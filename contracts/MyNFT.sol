5c9b33b1c5491ca8fd653d8c6048d4036abd9d04
Devi Amaolo <deviamaolo@gmail.com>
2023-01-18 02:43:24 -0300

    setup project

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract MyNFT {
+    constructor() {
+        console.log("this is my NFT");
+    }
+}
