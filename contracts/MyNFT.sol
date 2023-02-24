bd73d4625fb69b4d1874ec769db4c4d93e0e86cc
Devi Amaolo <deviamaolo@gmail.com>
2023-02-24 00:38:14 -0300

    my NFT contract

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -3,9 +3,31 @@
 pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT {
-    constructor() {
+contract MyNFT is ERC721 {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("SquareNFT,", "SQUARE") {
         console.log("this is my NFT");
     }
+
+    function makeNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        tokenURI(newItemId);
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns(string memory) {
+        require(_exists(_tokenId));
+        return "blah";
+    }
+
+
 }
